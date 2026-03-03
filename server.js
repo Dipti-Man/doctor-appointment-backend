19ee0adeb16cda67c52ccb413b42f0c4038ec974
Diptiman Singha <diptimansingha1@gmail.com>
2026-03-03 16:05:57 +0530

    Configured backend for Vercel deployment

--- server.js
+++ server.js
@@ -32,4 +32,13 @@ app.get("/", (req, res) => {
   res.send("API Working")
 });
 
-app.listen(port, () => console.log("Server started" , port))
+// app.listen(port, () => console.log("Server started" , port))  thora chcnge for vercel deployment
+
+if (process.env.NODE_ENV !== "production") {
+  const port = process.env.PORT || 4000;
+  app.listen(port, () => {
+    console.log("Server started on port", port);
+  });
+}
+
+export default app
